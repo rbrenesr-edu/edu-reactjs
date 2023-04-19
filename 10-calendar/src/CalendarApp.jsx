f2667e0c2ea1045abb968b32fda8a7e6f8aee9a9
Rafael Brenes <rbrenesr@gmail.com>
2023-04-19 00:50:32 -0600

    CalendarApp Cap. 347 Routes

---
+++ 10-calendar/src/CalendarApp.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from "./router"
+
+export const CalendarApp = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
+  )
+}
