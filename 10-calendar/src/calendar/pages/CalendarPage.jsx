cf80b0d90e754708ab8418257450aa88018066f5
Rafael Brenes <rbrenesr@gmail.com>
2023-04-19 13:16:43 -0600

    CalendarApp Cap. 349 react-big-calendar added

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,50 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
 import { Navbar } from "../"
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const myEventsList = [
+  {
+    title:'Titulo del evento',
+    notes: 'Notas del evento',
+    start: new Date(),
+    end: addHours( new Date(),2 ),
+    bgColor: '#fafafa',
+    user:{
+      id: '123465',
+      nombre:'Rafael Brenes'
+    }
+  }
+]
+
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar/>
+
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+    />
+
     </>
   )
 }
