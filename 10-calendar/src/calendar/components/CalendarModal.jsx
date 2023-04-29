4adc51fe78235ba344d395bc3bc332f81b1284be
Rafael Brenes <rbrenesr@gmail.com>
2023-04-29 09:23:22 -0600

    CalendarApp Cap. 354 Contenido del modal [RB]

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,47 +1,122 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
+import { useState } from "react";
+import { addHours } from "date-fns";
+import Modal from "react-modal";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export const CalendarModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
-const [isModalOpen, setIsModalOpen] = useState(true)
+  const [formValues, setFormValues] = useState({
+    title: "title",
+    notes: "notes",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  });
 
+  const onInputChanged = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
 
-const  onCloseModal = () => { 
-    console.log("cerrando modal"); 
+  const onDateChanged = (event, changin) => {
+    setFormValues({
+      ...formValues,
+      [changin]: event
+    });
+  };
+
+  const onCloseModal = () => {
+    console.log("cerrando modal");
     setIsModalOpen(false);
-}
-
+  };
 
   return (
     <Modal
-        isOpen={ isModalOpen }        
-        onRequestClose={ onCloseModal }
-        style={customStyles}
-        contentLabel="Example Modal"
-        className="modal"
-        overlayClassName="modal-fondo"
-        closeTimeoutMS={ 200 }
+      isOpen={isModalOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+      className="modal"
+      overlayClassName="modal-fondo"
+      closeTimeoutMS={200}
     >
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <DatePicker            
+            selected={formValues.start}
+            onChange={  (event)=> onDateChanged (event, 'start') }
+            className="form-control"
+            dateFormat="Pp"
+          />
+        </div>
 
-        <h1>Modal</h1>
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <DatePicker            
+            selected={formValues.end}
+            onChange={  (event)=> onDateChanged (event, 'end') }
+            className="form-control"
+            dateFormat="Pp"
+            minDate={ formValues.start }
+          />
+        </div>
+
         <hr />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, ex!
-        </p>
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={formValues.title}
+            onChange={onInputChanged}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
 
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={formValues.notes}
+            onChange={onInputChanged}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
-  )
-}
+  );
+};
