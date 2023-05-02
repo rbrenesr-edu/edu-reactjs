3d287cef71c0707863ea1a7d3bad682f56e31ad4
Rafael Brenes <rbrenesr@gmail.com>
2023-05-02 11:02:42 -0600

    CalendarApp Cap.358 Instalacion y configuracion de Redux (proceso) [RB]

---
+++ 10-calendar/src/store/ui/uiSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const uiSlice = createSlice({
+   name: 'ui',
+   initialState: {
+      isDateModalOpen: false
+   },
+   reducers: {
+      onOpenDateModal: (state, /* action */ ) => {
+         state.isDateModalOpen = true;
+
+         //* si no se utiliza redux-toolkit, no se puede realizar código mutante del estado, por lo que se debería hacer:         
+        //  return {
+        //     ...state,
+        //     isDateModalOpen : true
+        //  }
+
+
+      },
+      onCloseDateModal: (state) => {
+        state.isDateModalOpen = false
+      }
+   }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { onOpenDateModal, onCloseDateModal } = uiSlice.actions;
+
