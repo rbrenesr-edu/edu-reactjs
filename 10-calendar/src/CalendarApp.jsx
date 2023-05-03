1d111846df18d36020e7d332089b04d82b6f41be
Rafael Brenes <rbrenesr@gmail.com>
2023-05-03 06:35:25 -0600

    CalendarApp Cap.358 Instalacion y configuracion de Redux

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -5,7 +5,7 @@ import { store } from "./store";
 
 export const CalendarApp = () => {
   return (
-    <Provider store={store}>
+    <Provider store={ store } >
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
