40e125d390ee05c3c606a448020e240bc17b5e6b
Rafael Brenes <rbrenesr@gmail.com>
2023-05-20 23:05:20 -0600

    CalendarApp´-Backend Cap.372 COnfigurando Express [RB]

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo!!!');
+const express = require('express');
+
+const app = express()
+
+ app.get('/', function (req, res) {
+  res.json({
+    'ok':true
+  });    
+})
+
+app.listen(4000, ()=>{
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+} );
