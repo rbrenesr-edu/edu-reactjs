8d124b0bd6200427d698b3a59d8a32aad72b2586
Rafael Brenes <rbrenesr@gmail.com>
2023-05-24 22:55:19 -0600

    CalednarApp-Backend Cap.382 Validaciones de usuario [RB]

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -15,7 +15,7 @@ const loginUsuario = (req, res = response) => {
 const crearUsuario = async (req, res = response) => {
   const { name, email, password } = req.body;
 
-  // comentario
+  //* En esta sección es posible hacer validaciones, pero se recomienda usar express-validator directamente en los routes
   //   if(name.length < 5){
   //      return res.status(400).json( {
   //         ok:false,
@@ -25,20 +25,32 @@ const crearUsuario = async (req, res = response) => {
   // comentario
 
   try {
-    const usuario = new Usuario(req.body);
+
+    //*Validar si el user email ya existe en la base de datos
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El email ya se encuentra registrado"
+      });
+    }
+
+
+    //* Desde acá se puede ingresar un nuevo usuario.
+    usuario = new Usuario(req.body);
     await usuario.save();
 
     res.status(201).json({
       ok: true,
       msg: "registro",
-      name,
-      email,
-      password,
+      uid: usuario.id,
+      name: usuario.name,      
     });
   } catch (error) {
-    
-    console.log(error);
 
+    console.log('error personalizado:  ' + error);
+     
     res.status(500).json({
       ok: false,
       msg: "Verificar con admin"
